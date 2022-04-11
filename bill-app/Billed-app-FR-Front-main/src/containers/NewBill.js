2ab86b17d9a45c9376030ec524258ff50d3f1251
Clément Théraud <theraud.clement@gmail.com>
2022-04-11 14:43:39 +0200

    img format

--- bill-app/Billed-app-FR-Front-main/src/containers/NewBill.js
+++ bill-app/Billed-app-FR-Front-main/src/containers/NewBill.js
@@ -15,15 +15,23 @@ export default class NewBill {
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
+
+
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    //MODIF img format accepted
+    const input = this.document.querySelector(`input[data-testid="file"]`)
+    const file = input.files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
     formData.append('email', email)
+    //Img formats supported
+    const imgFormats = ['image/jpeg', 'image/jpg', 'image/png']
+
+
 
     this.store
       .bills()
@@ -42,7 +50,7 @@ export default class NewBill {
   }
   handleSubmit = e => {
     e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+    //console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
       email,
